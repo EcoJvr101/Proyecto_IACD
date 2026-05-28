e0d2c0962bf1d91f6940c6f537ad23d45cce450d
EcoJvr101 <jorgealtamirano5a@gmail.com>
2026-05-28 09:14:15 +0100

    Subida de mapin.py para ejecucion conjunta

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import data_cleaning
+import data_integration
+import eda_analysis
+import clustering
+import predictive_modeling
+
+def main():
+    print("="*40)
+    print(f"PIPELINE DE CIÊNCIA DE DADOS")
+    print("="*40)
+
+    try:
+        
+        #Data Integration
+        print("\n>>> Integração de Dados <<<\n")
+        data_integration.main()
+
+        #Data Cleaning
+        print("\n>>> Limpeza de Dados <<<\n")
+        data_cleaning.main()
+
+        #Data Analysis
+        print("\n>>> Análise EDA <<<\n")
+        eda_analysis.main()
+
+        #Clustering
+        print("\n>>> Algoritmo K-Means <<<\n")
+        clustering.main()
+
+        #PRedictive Model
+        print("\n>>> Modelo Preditivo <<<\n")
+        predictive_modeling.main()
+
+        print("="*90)
+        print(f"PIPELINE DE CIÊNCIA DE DADOS EXECUTADO SEM ERROS")
+        print(f"Representação visual adicional dos dados localizada em 'visualizaciones'")
+        print("="*90)
+    
+    except Exception as e:
+        print(f"\n[ERRO]: pipeline interrompido em {e}]\n")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
